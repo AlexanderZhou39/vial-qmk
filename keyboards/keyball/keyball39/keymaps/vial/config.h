7767ca84eaa77a54b99bbcf3e0043c991bd8f62f
Alex <alexzhou3999@gmail.com>
2024-12-05 01:57:05 -0800

    add keyball

---
+++ keyboards/keyball/keyball39/keymaps/vial/config.h
@@ -0,0 +1,58 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2022 @Yowkees
+Copyright 2022 MURAOKA Taro (aka KoRoN, @kaoriya)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#pragma once
+/* any side can be master by default, enable split sync to support it */
+//#define MASTER_LEFT
+#define MASTER_RIGHT
+//#define EE_HANDS
+
+/* Define layer number */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
+
+/* Maximum of combo, tap dance in vial */
+#define VIAL_TAP_DANCE_ENTRIES 32
+#define VIAL_COMBO_ENTRIES 32
+#define VIAL_KEY_OVERRIDE_ENTRIES 32
+#define VIAL_USER_ENTRIES 32
+
+#define VIAL_KEYBOARD_UID {0xA5, 0x0A, 0x70, 0xB0, 0xC9, 0x2C, 0x4E, 0x84}
+
+
+
+#ifdef RGBLIGHT_ENABLE
+#    define RGBLIGHT_EFFECT_BREATHING
+#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#    define RGBLIGHT_EFFECT_SNAKE
+#    define RGBLIGHT_EFFECT_KNIGHT
+#    define RGBLIGHT_EFFECT_CHRISTMAS
+#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#    define RGBLIGHT_EFFECT_RGB_TEST
+#    define RGBLIGHT_EFFECT_ALTERNATING
+#    define RGBLIGHT_EFFECT_TWINKLE
+#endif
+
+#define TAP_CODE_DELAY 5
+
+
+#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
+#define AUTO_MOUSE_DEFAULT_LAYER 1
---
+++ keyboards/keyball/keyball39/keymaps/vial/rules.mk
@@ -0,0 +1,18 @@
+RGBLIGHT_ENABLE = yes
+
+OLED_ENABLE = yes
+
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIAL_INSECURE = yes
+
+
+# Reduce size of Vial
+QMK_SETTINGS = yes
+COMBO_ENABLE = yes
+TAP_DANCE_ENABLE = yes 
+KEY_OVERRIDE_ENABLE = yes
+
+VIALRGB_ENABLE = no 
+#	"lighting": "vialrgb",
+
